294689191787e1c4bf004f9ad8fbd6d31dc42e44
zonecc <bkevery@gmail.com>
2019-04-14 15:14:37 +0300

    force update from final-gh

--- js/nav-script.js
+++ js/nav-script.js
@@ -1,14 +1,14 @@
-const navBar = document.querySelector(".main-header");
-window.addEventListener("load", e => {
-  navBar.setAttribute("style", "background-color : transparent");
-});
+// const navBar = document.querySelector(".main-header");
+// window.addEventListener("load", e => {
+//   navBar.setAttribute("style", "background-color : transparent");
+// });
 
-window.addEventListener("scroll", e => {
-  navBar.setAttribute("style", "background-color : transparent");
-  if (window.pageYOffset >= 420) {
-    navBar.setAttribute("style", "background-color : #0761867d;");
-  }
-});
+// window.addEventListener("scroll", e => {
+//   navBar.setAttribute("style", "background-color : transparent");
+//   if (window.pageYOffset >= 420) {
+//     navBar.setAttribute("style", "background-color : #0761867d;");
+//   }
+// });
 
 const slideArray = document.querySelectorAll(".slides");
 const nextSlide = document.querySelector(".toggle-right");
@@ -17,8 +17,8 @@ const sliderBox = document.querySelector("section#slider");
 
 let sliderController = direction => {
   const currentSlide = document.querySelector("div.slides.active");
-  let nextSlideIndex = parseInt(currentSlide.classList[0].split("-")[1]);
-  let prevSlideIndex = parseInt(currentSlide.classList[0].split("-")[1]) - 2;
+  let nextSlideIndex = parseInt(currentSlide.classList[1].split("-")[1]);
+  let prevSlideIndex = parseInt(currentSlide.classList[1].split("-")[1]) - 2;
   const maxSlideIndex = slideArray.length - 1;
   const minSlideIndex = 0;
   if (nextSlideIndex >= slideArray.length) {
