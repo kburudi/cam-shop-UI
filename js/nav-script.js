651f819a9f18ab1476a448ce1919b95e1e36c975
zonecc <bkevery@gmail.com>
2019-04-14 18:57:53 +0300

    autoload for slider

--- js/nav-script.js
+++ js/nav-script.js
@@ -1,7 +1,4 @@
 // const navBar = document.querySelector(".main-header");
-// window.addEventListener("load", e => {
-//   navBar.setAttribute("style", "background-color : transparent");
-// });
 
 // window.addEventListener("scroll", e => {
 //   navBar.setAttribute("style", "background-color : transparent");
@@ -17,6 +14,7 @@ const sliderBox = document.querySelector("section#slider");
 
 let sliderController = direction => {
   const currentSlide = document.querySelector("div.slides.active");
+  let currentSlideIndex = parseInt(currentSlide.classList[1].split("-")[1]) - 1;
   let nextSlideIndex = parseInt(currentSlide.classList[1].split("-")[1]);
   let prevSlideIndex = parseInt(currentSlide.classList[1].split("-")[1]) - 2;
   const maxSlideIndex = slideArray.length - 1;
@@ -32,17 +30,42 @@ let sliderController = direction => {
   currentSlide.classList.remove("active");
   if (direction === "next") {
     slideArray[nextSlideIndex].classList.add("active");
-  } else {
+  } else if (direction === "prev") {
     slideArray[prevSlideIndex].classList.add("active");
   }
 };
 
+// window.addEventListener("load", e => {
+//   // navBar.setAttribute("style", "background-color : transparent");
+//   startLoad();
+// });
+let autoSlide = setInterval(myTimer, 4000);
+
+function myTimer() {
+  sliderController("next");
+}
+
+function stopAutoload() {
+  clearInterval(autoSlide);
+}
+
+let restartSlider = () => {
+  setTimeout(function() {
+    autoSlide = setInterval(myTimer, 4000);
+  }, 3000);
+};
+
+// sliderController("auto");
 nextSlide.addEventListener("click", e => {
   e.preventDefault();
+  stopAutoload();
   sliderController("next");
+  restartSlider();
 });
 
 prevSlide.addEventListener("click", e => {
   e.preventDefault();
+  stopAutoload();
   sliderController("prev");
+  restartSlider();
 });
